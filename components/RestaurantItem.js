c77a0ec8d07b679208850327a9a009466c686fc5
Muhammad Asandy Putra <asandyputra77@gmail.com>
2024-04-29 08:52:49 +0800

    Style Commit2

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -3,11 +3,15 @@ import Link from 'next/link';
 import PropTypes from 'prop-types';
 import Card from './styled/Card';
 import Container from './styled/Container';
+import Image from './styled/Image';
  
 function RestaurantItem({ id, pictureId, name, description }) {
   return (
     <Card>
-      <img src={`https://restaurant-api.dicoding.dev/images/small/${pictureId}`} alt={name} />
+      <Image
+        height="250px"
+        src={`https://restaurant-api.dicoding.dev/images/small/${pictureId}`}
+        alt={name} />
  
       <Container>
         <h2><Link href={`/detail/${id}`}>{name}</Link></h2>
